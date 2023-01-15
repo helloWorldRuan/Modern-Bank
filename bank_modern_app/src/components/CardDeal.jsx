3248507389a34d7aeab7fffc4f6d4b30219de4c6
helloWorldRuan <ruan.rodrigues@sptech.school>
2023-01-15 13:15:17 -0300

    Billing, CardDeal, Testimonials & Clients sections added

--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={`${layout.section} px-16`}>
+    <div className={`${layout.sectionInfo} w-[50%]`}>
+      <h2 className={styles.heading2}>
+        Find a better card deal
+        <br className="sm:block hidden" />
+        in few easy steps.
+      </h2>
 
-export default CardDeal
+      <p className={`${styles.paragraph} max-w-[550px] mt-5`}>
+        Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor. Aliquet ultrices ac, ametau.
+        <Button styles="mt-10"/>
+      </p>
+    </div>
+
+    <div className="w-[50%]">
+      <img src={card} alt="card" className="w-[100%]"/>
+    </div>
+  </section>
+);
+
+export default CardDeal;
